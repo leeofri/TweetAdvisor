f5faef0d217f78e84ee85c558315419432c8e608
Lee <leeofri@gmail.com>
2016-04-19 21:23:28 +0300

    downgrade dotnet

--- StockAnalyzer.Site/Areas/HelpPage/SampleGeneration/HelpPageSampleGenerator.cs
+++ StockAnalyzer.Site/Areas/HelpPage/SampleGeneration/HelpPageSampleGenerator.cs
@@ -10,7 +10,6 @@ using System.Net.Http;
 using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
 using System.Web.Http.Description;
-using System.Xml.Linq;
 using Newtonsoft.Json;
 
 namespace StockAnalyzer.Site.Areas.HelpPage
@@ -392,8 +391,8 @@ namespace StockAnalyzer.Site.Areas.HelpPage
         {
             try
             {
-                XDocument xml = XDocument.Parse(str);
-                return xml.ToString();
+                //XDocument xml = XDocument.Parse(str);
+                return null;//xml.ToString();
             }
             catch
             {
